b7cf853a0def23fd7eb46cb600e2f927989a03b1
denverhogan <hogandenver05@gmail.com>
2025-08-25 16:03:51 -0400

    Main Activity UI Updates (#1)
    * Add button to UI
    
    * Add button logic

--- app/src/main/java/com/denverhogan/themeparks/MainActivity.kt
+++ app/src/main/java/com/denverhogan/themeparks/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.denverhogan.themeparks
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.hello_world_text)
+        button = findViewById(R.id.button)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        button.setOnClickListener {
+            onButtonClicked()
+        }
+    }
+
+    fun onButtonClicked() {
+        Log.d(LOG_TAG, "Button clicked!")
+        Log.e(LOG_TAG, "Button clicked!")
+
+        textView.text = getString(R.string.goodbye_world)
+    }
+
+    companion object {
+        private const val LOG_TAG = "MainActivity"
     }
 }
