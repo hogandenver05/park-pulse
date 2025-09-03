91b9fd6efe8108d2a022db9ac9de01db111f03d1
Denver Hogan <denver.hogan@fox.com>
2025-09-03 10:54:34 -0400

    Show destination on item click

---
+++ app/src/main/java/com/denverhogan/themeparks/detail/DestinationDetailFragment.kt
@@ -0,0 +1,57 @@
+package com.denverhogan.themeparks.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.denverhogan.themeparks.databinding.FragmentDestinationDetailBinding
+
+class DestinationDetailFragment : Fragment() {
+    private var _binding: FragmentDestinationDetailBinding? = null
+    private val binding get() = _binding!!
+
+    private var id: Int? = null
+    private var name: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            id = it.getInt(ARG_ID)
+            name = it.getString(ARG_NAME)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDestinationDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.destinationName.text = name ?: "An error occurred"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        private const val ARG_ID = "id"
+        private const val ARG_NAME = "name"
+
+        @JvmStatic
+        fun newInstance(id: Long, name: String) = DestinationDetailFragment().apply {
+            arguments = Bundle().apply {
+                putLong(ARG_ID, id)
+                putString(ARG_NAME, name)
+            }
+        }
+    }
+}
