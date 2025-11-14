233058fdb42b327a6f3a5ad453720dcb8556590a
hogandenver05 <hogandenver05@gmail.com>
2025-11-14 00:00:44 -0500

    remove unused files

--- app/src/main/java/com/denverhogan/parkpulse/network/GetAllRidesResult.kt
+++
@@ -1,8 +0,0 @@
-package com.denverhogan.parkpulse.network
-
-import com.denverhogan.parkpulse.model.Ride
-
-sealed interface GetAllRidesResult {
-    data class Success(val rides: List<Ride>) : GetAllRidesResult
-    data class Error(val message: String) : GetAllRidesResult
-}
