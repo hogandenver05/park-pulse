6ff2a2d98ad23a08011590ca39b763af31339d90
Denver Hogan <hogandenver05@gmail.com>
2025-11-17 07:32:08 -0500

    Implement Sorting for Parks and Rides (#11)
    * add favorites, alphabetical and location-based parks list sorting
    
    * add wait time-based and alphabetical rides list sorting
    
    * refactor UI

--- app/src/main/java/com/denverhogan/parkpulse/network/GetAllParksResult.kt
+++
@@ -1,8 +0,0 @@
-package com.denverhogan.parkpulse.network
-
-import com.denverhogan.parkpulse.model.Park
-
-sealed interface GetAllParksResult {
-    data class Success(val parks: List<Park>) : GetAllParksResult
-    data class Error(val message: String) : GetAllParksResult
-}
--- app/src/main/java/com/denverhogan/parkpulse/ui/theme/Color.kt
+++ app/src/main/java/com/denverhogan/parkpulse/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val WaitTimeGreen = Color(0xFFA5D6A7)
+val WaitTimeYellow = Color(0xFFFFF59D)
+val WaitTimeRed = Color(0xFFEF9A9A)
+val RideClosedGrey = Color(0xFFBDBDBD)
