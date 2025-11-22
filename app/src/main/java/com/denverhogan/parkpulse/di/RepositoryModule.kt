6ed347ed60caf7dddf202ed5ef7d7b6c324a7304
hogandenver05 <hogandenver05@gmail.com>
2025-11-22 12:53:14 -0500

    Fix repository DI to use ParkRepository with proper dependencies and add coordinate parsing error handling

--- app/src/main/java/com/denverhogan/parkpulse/di/RepositoryModule.kt
+++ app/src/main/java/com/denverhogan/parkpulse/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.denverhogan.parkpulse.di
 
+import com.denverhogan.parkpulse.data.ParkRepository
+import com.denverhogan.parkpulse.data.local.FavoriteParkDao
+import com.denverhogan.parkpulse.location.LocationProvider
 import com.denverhogan.parkpulse.network.QueueTimesApi
-import com.denverhogan.parkpulse.repository.ParksRepository
 import com.denverhogan.parkpulse.repository.RidesRepository
 import dagger.Module
 import dagger.Provides
@@ -13,8 +15,12 @@ import dagger.hilt.components.SingletonComponent
 object RepositoryModule {
 
     @Provides
-    fun provideParksRepository(api: QueueTimesApi): ParksRepository {
-        return ParksRepository(api)
+    fun provideParkRepository(
+        api: QueueTimesApi,
+        favoriteParkDao: FavoriteParkDao,
+        locationProvider: LocationProvider
+    ): ParkRepository {
+        return ParkRepository(api, favoriteParkDao, locationProvider)
     }
 
     @Provides
